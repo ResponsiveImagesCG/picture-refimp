95e63f284288e333b395fd4fc67b32077fddc689
Marcos Caceres <marcos@marcosc.com>
2013-06-05 16:42:22 +0100

    fixed grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        src: [
+          'lib/*.js',
+          'lib/**/*.js'
+        ],
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['lib/**/*.js', 'test/*-tests.js']
+    },
+
+    qunit: {
+      options: {
+        timeout: 20000
+      },
+      all: {
+        options: {
+          urls: ['http://localhost:8000/test/index.html']
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        port: 8000,
+        base: '.'
+      }
+    },
+
+    jsbeautifier: {
+      files: ['lib/**/*.js', 'test/*-tests.js']
+    },
+
+    jsdoc : {
+        dist : {
+            src: ['lib/**/*.js', 'test/*.js'], 
+            options: {
+                destination: 'doc'
+            }
+        }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.registerTask('test', ['connect', 'qunit']);
+  grunt.registerTask('default', ['jsbeautifier', 'jshint', 'test', 'uglify']);
+
+};
